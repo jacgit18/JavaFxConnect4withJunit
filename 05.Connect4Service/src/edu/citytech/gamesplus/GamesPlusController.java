73f746e1fe9c8ac3ab8fa3087edd143e8e731158
JavaFx <JavaFx@10.10.198.52>
2019-11-07 17:36:18 -0500

    added code

--- 05.Connect4Service/src/edu/citytech/gamesplus/GamesPlusController.java
+++ 05.Connect4Service/src/edu/citytech/gamesplus/GamesPlusController.java
@@ -1,5 +1,8 @@
 package edu.citytech.gamesplus;
 
+import edu.citytech.dao.WinningCombo;
+import edu.citytech.dao.WinningComboDAO;
+import edu.citytech.service.Connect4Service;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -159,6 +162,90 @@ public class GamesPlusController {
     @FXML
     void onMouseClicked(MouseEvent event) {
     	
+    	Connect4Service win = new Connect4Service();
+//    	win.
+    	
+    	
+    	
+    	// try to iterate to set values to 
+//    	for (int i = 0; i < array.length; i++) {
+			
+//		}
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	circ_1.setUserData("?");
+    	circ_2.setUserData("?");
+    	circ_3.setUserData("?");
+    	circ_3.setUserData("?");
+    	circ_4.setUserData("?");
+    	circ_5.setUserData("?");
+    	circ_6.setUserData("?");
+    	circ_7.setUserData("?");
+    	circ_8.setUserData("?");
+    	circ_9.setUserData("?");
+    	circ_10.setUserData("?");
+    	circ_11.setUserData("?");
+    	circ_12.setUserData("?");
+    	circ_13.setUserData("?");
+    	circ_14.setUserData("?");
+    	circ_15.setUserData("?");
+    	circ_16.setUserData("?");
+    	circ_17.setUserData("?");
+    	circ_18.setUserData("?");
+    	circ_19.setUserData("?");
+    	circ_20.setUserData("?");
+    	circ_21.setUserData("?");
+    	circ_22.setUserData("?");
+    	circ_23.setUserData("?");
+    	circ_24.setUserData("?");
+    	circ_25.setUserData("?");
+    	circ_26.setUserData("?");
+    	circ_27.setUserData("?");
+    	circ_28.setUserData("?");
+    	circ_29.setUserData("?");
+    	circ_30.setUserData("?");
+    	circ_31.setUserData("?");
+    	circ_32.setUserData("?");
+    	circ_33.setUserData("?");
+    	circ_33.setUserData("?");
+    	circ_34.setUserData("?");
+    	circ_35.setUserData("?");
+    	circ_36.setUserData("?");
+    	
+
+    	
+//    	Circle circ = (Circle) event.getSource();
+//    	circ = new
+//    	circ_1.getUserData();
+//    	String getp1 = (String) circ_1.getUserData();
+//    	Boolean setcolor =  circ_1.setFill(Color.WHITE);
+
+    	
+    	Circle circ = (Circle) event.getSource();
+    	if (!circ.getUserData().equals("?")) {   
+    		String value = "illegal";
+    		lbl_Message.setText(value);
+	 
+    		return;
+    	}
+    	
+    	//try testing colors
+//    	String nextMove = isX ? "X":"O";
+//    	circ.setUserData(nextMove);
+//    	isX = !isX;
+//    	
+//    	
+//    	 String p1,p2,p3;
+//    	 p1 = circ1.getUserData();
+    	 
+//    	tostring 
+//    	or String
+    	
 
     	circ_1.setFill(Color.RED);
     	circ_2.setFill(Color.RED);
@@ -201,49 +288,46 @@ public class GamesPlusController {
     	circ_36.setFill(Color.RED);
     	
     	
-//		if () {
-//					
-//				}
     	
-    	circ_1.setFill(Color.YELLOW);
-    	circ_2.setFill(Color.YELLOW);
-    	circ_3.setFill(Color.YELLOW);
-    	circ_4.setFill(Color.YELLOW);
-    	circ_5.setFill(Color.YELLOW);
-    	circ_6.setFill(Color.YELLOW);
-    	circ_7.setFill(Color.YELLOW);
-    	circ_8.setFill(Color.YELLOW);
-    	circ_9.setFill(Color.YELLOW);
-    	
-    	circ_10.setFill(Color.YELLOW);
-    	circ_11.setFill(Color.YELLOW);
-    	circ_12.setFill(Color.YELLOW);
-    	circ_13.setFill(Color.YELLOW);
-    	circ_14.setFill(Color.YELLOW);
-    	circ_15.setFill(Color.YELLOW);
-    	circ_16.setFill(Color.YELLOW);
-    	circ_17.setFill(Color.YELLOW);
-    	circ_18.setFill(Color.YELLOW);
-    	
-    	circ_19.setFill(Color.YELLOW);
-    	circ_20.setFill(Color.YELLOW);
-    	circ_21.setFill(Color.YELLOW);
-    	circ_22.setFill(Color.YELLOW);
-    	circ_23.setFill(Color.YELLOW);
-    	circ_24.setFill(Color.YELLOW);
-    	circ_25.setFill(Color.YELLOW);
-    	circ_26.setFill(Color.YELLOW);
-    	circ_27.setFill(Color.YELLOW);
-    	
-    	circ_28.setFill(Color.YELLOW);
-    	circ_29.setFill(Color.YELLOW);
-    	circ_30.setFill(Color.YELLOW);
-    	circ_31.setFill(Color.YELLOW);
-    	circ_32.setFill(Color.YELLOW);
-    	circ_33.setFill(Color.YELLOW);
-    	circ_34.setFill(Color.YELLOW);
-    	circ_35.setFill(Color.YELLOW);
-    	circ_36.setFill(Color.YELLOW);
+//    	circ_1.setFill(Color.YELLOW);
+//    	circ_2.setFill(Color.YELLOW);
+//    	circ_3.setFill(Color.YELLOW);
+//    	circ_4.setFill(Color.YELLOW);
+//    	circ_5.setFill(Color.YELLOW);
+//    	circ_6.setFill(Color.YELLOW);
+//    	circ_7.setFill(Color.YELLOW);
+//    	circ_8.setFill(Color.YELLOW);
+//    	circ_9.setFill(Color.YELLOW);
+//    	
+//    	circ_10.setFill(Color.YELLOW);
+//    	circ_11.setFill(Color.YELLOW);
+//    	circ_12.setFill(Color.YELLOW);
+//    	circ_13.setFill(Color.YELLOW);
+//    	circ_14.setFill(Color.YELLOW);
+//    	circ_15.setFill(Color.YELLOW);
+//    	circ_16.setFill(Color.YELLOW);
+//    	circ_17.setFill(Color.YELLOW);
+//    	circ_18.setFill(Color.YELLOW);
+//    	
+//    	circ_19.setFill(Color.YELLOW);
+//    	circ_20.setFill(Color.YELLOW);
+//    	circ_21.setFill(Color.YELLOW);
+//    	circ_22.setFill(Color.YELLOW);
+//    	circ_23.setFill(Color.YELLOW);
+//    	circ_24.setFill(Color.YELLOW);
+//    	circ_25.setFill(Color.YELLOW);
+//    	circ_26.setFill(Color.YELLOW);
+//    	circ_27.setFill(Color.YELLOW);
+//    	
+//    	circ_28.setFill(Color.YELLOW);
+//    	circ_29.setFill(Color.YELLOW);
+//    	circ_30.setFill(Color.YELLOW);
+//    	circ_31.setFill(Color.YELLOW);
+//    	circ_32.setFill(Color.YELLOW);
+//    	circ_33.setFill(Color.YELLOW);
+//    	circ_34.setFill(Color.YELLOW);
+//    	circ_35.setFill(Color.YELLOW);
+//    	circ_36.setFill(Color.YELLOW);
     	
     	
     	
