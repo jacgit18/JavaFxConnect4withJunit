9e680c596f3f1c330127945fae3b2c657b0ec71a
JavaFx <JavaFx@10.10.198.52>
2019-11-12 17:14:09 -0500

    added code

--- 05.Connect4Service/src/edu/citytech/gamesplus/GamesPlusController.java
+++ 05.Connect4Service/src/edu/citytech/gamesplus/GamesPlusController.java
@@ -22,118 +22,113 @@ import javafx.scene.shape.StrokeType;
 
 public class GamesPlusController implements Initializable {
 
-    @FXML
-    private GridPane fpGames2;
+	@FXML
+	private GridPane fpGames2;
 
-    @FXML
-    private Label Title;
+	@FXML
+	private Label Title;
 
-    @FXML
-    private Label Name;
+	@FXML
+	private Label Name;
 
-    @FXML
-    private Label lbl_Message;
+	@FXML
+	private Label lbl_Message;
 
-    @FXML
-    private Button btn_Reset;
-  
+	@FXML
+	private Button btn_Reset;
+
 	@FXML
 	void checkFloatOption(ActionEvent event) {
-		
+
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle resource) {
 		int i = 0;
-		for (int Rows = 0; Rows <= 5; Rows++) 
+		for (int Rows = 0; Rows <= 5; Rows++)
 			for (int Coulmn = 0; Coulmn <= 6; Coulmn++) {
 				Circle circle = new Circle();
 				circles[i] = circle;
-		    	circle.setCenterX(100.0f);
-		    	circle.setCenterY(100.0f);
-		    	circle.setRadius(45.0f);
-		    	circle.setFill(Color.WHITE);
-		    	circle.setStroke(Color.rgb(18, 97, 183));
-		    	circle.setStrokeWidth(5);
-		    	circle.setStrokeType(StrokeType.OUTSIDE);
-		    	circle.setStrokeLineCap(StrokeLineCap.ROUND);
-		    	circle.setStrokeLineJoin(StrokeLineJoin.ROUND);
-		    	circle.setStrokeMiterLimit(10);
-		    	circle.setStrokeDashOffset(0);
-	
-		    	circle.setUserData(i);
-		    	circle.setOnMouseClicked((MouseEvent e) -> this.clickedEvent(e));
-		    	fpGames2.add(circle, Coulmn, Rows);
-		    	i++;
+				circle.setCenterX(100.0f);
+				circle.setCenterY(100.0f);
+				circle.setRadius(45.0f);
+				circle.setFill(Color.WHITE);
+				circle.setStroke(Color.rgb(18, 97, 183));
+				circle.setStrokeWidth(5);
+				circle.setStrokeType(StrokeType.OUTSIDE);
+				circle.setStrokeLineCap(StrokeLineCap.ROUND);
+				circle.setStrokeLineJoin(StrokeLineJoin.ROUND);
+				circle.setStrokeMiterLimit(10);
+				circle.setStrokeDashOffset(0);
+
+				circle.setUserData(i);
+				circle.setOnMouseClicked((MouseEvent e) -> this.clickedEvent(e));
+				fpGames2.add(circle, Coulmn, Rows);
+				i++;
 			}
-			
+
 	}
+
 	private boolean isX = true;
-    private void clickedEvent(MouseEvent e) {
-    	Circle circle = (Circle) e.getSource();
-    	
-    	if (!circle.getUserData().equals("?")) {
 
-    	}
-    	
-    	Data d  = new Data (d.index, d.XorO);
+	private void clickedEvent(MouseEvent e) {
+		Circle circle = (Circle) e.getSource();
 
-    	String XorO = "";
-    	if(isX) {
-    		XorO = "X";
-        	circle.setUserData(d);
-        	circle.setFill(Color.RED);
+		if (!circle.getUserData().equals("?")) {
 
-    	}
-    	else {
+		}
+		
+		int cellNumber = Integer.parseInt( String.valueOf( circle.getUserData()));
+
+		String XorO = "";
+		if (isX) {
+			XorO = "X";
+			circle.setUserData(XorO);
+			circle.setFill(Color.RED);
+
+		} else {
 			XorO = "O";
-	    	circle.setUserData(d);
-	    	circle.setFill(Color.YELLOW);
-
+			circle.setUserData(XorO);
+			circle.setFill(Color.YELLOW);
 
 		}
-    	isX = !isX;
-    	circle.setUserData(XorO);
-    	String [] moves = new String[42];
-    	
-    	for (int i = 0; i < circles.length; i++) {
-			moves[i] = (String) circles[i].getUserData();//
-		}
-    	
-    	int cellNumber = (int) circle.getUserData();
-    	String newMove = (String) circle.getUserData(); //
-    	moves[cellNumber] = "?";
-    	circle.setUserData("?");
-    	
-    	int nextMove = Connect4Service.getValidCellMoves(moves, cellNumber);
-    	
-    	circles[nextMove].setUserData(newMove);
-    	circles[nextMove].getStyleClass().add(XorO);
+		isX = !isX;
+		circle.setUserData(XorO);
+		String[] moves = new String[42];
 
-    	
-    	lbl_Message.setText("Current index " + cellNumber + "next valid move is " + nextMove + "Clicked on" + new Date());
+		for (int i = 0; i < circles.length; i++) {
+			moves[i] = String.valueOf(circles[i].getUserData());
+		}
+
+		String newMove = (String) circle.getUserData(); //
+		moves[cellNumber] = "?";
+		circle.setUserData("?");
+
+		int nextMove = Connect4Service.getValidCellMoves(moves, cellNumber);
+
+		circles[nextMove].setUserData(newMove);
+		circles[nextMove].getStyleClass().add(XorO);
+
+		lbl_Message
+				.setText("Current index " + cellNumber + "next valid move is " + nextMove + "Clicked on" + new Date());
 //    	lbl_Message.setText("Clicked on" + new Date());
 
-    }
-	
-    private Circle[] circles = new Circle[42];
-
-    @FXML
-    void reset(ActionEvent event) {
-    	
-    for (Circle circle : circles) {
-    
-    	circle.setFill(Color.WHITE);
-
-    	circle.getStyleClass().removeIf(e -> e.equals("X") || e.equals("O"));
 	}
-	    isX = true;
+
+	private Circle[] circles = new Circle[42];
+
+	@FXML
+	void reset(ActionEvent event) {
+
+		for (Circle circle : circles) {
+
+			circle.setFill(Color.WHITE);
+
+			circle.getStyleClass().removeIf(e -> e.equals("X") || e.equals("O"));
+		}
+		isX = true;
 		lbl_Message.setText(" ");
 
-			 
-		 }
-		
-
-    
+	}
 
 }
