610af6b3605ab6b4d6ac63b8b4a77bd47f111371
jac <joshuacarpentier21@gmail.com>
2019-11-27 17:45:39 -0500

    added

--- 05.Connect4Service/src/edu/citytech/gamesplus/GamesPlusController1.java
+++ 05.Connect4Service/src/edu/citytech/gamesplus/GamesPlusController1.java
@@ -1,12 +1,18 @@
 package edu.citytech.gamesplus;
 
+import java.util.List;
+
+import edu.citytech.service.Connect4Service;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
 
 public class GamesPlusController1 {
@@ -152,14 +158,59 @@ public class GamesPlusController1 {
     @FXML
     private Button btn_Reset;
 
+	private boolean isRed = false;
+
+    
     
     @FXML
     void onMouseClicked(MouseEvent event) {
+    	Circle circle = (Circle) event.getSource();
 
-    }
+		if (circle.getFill() != Color.WHITE) {
+			lbl_Message.setText("This is not your turn!");
+			return;
+		}
 
+		// GET ALL CHILDREN HERE.
+		List<Node> nodes = fpGames.getChildren();
+
+		// MAP X OR O TO COLOR.
+		String[] rawMoves = nodes.stream().map(x -> {
+			Circle cir = (Circle) x;
+			Paint fillColor = cir.getFill();
+			return fillColor == Color.WHITE ? "?" : fillColor == Color.RED ? "X" : "O";
+		}).toArray(String[]::new);
+		
+		int id = Integer.parseInt(circle.getId().replace("circ_", ""));
+		int validMove = Connect4Service.getValidCellMoves(rawMoves, id);
+
+		Circle validCircle = (Circle) nodes.get(validMove);
+		Color color = isRed ? Color.RED : Color.YELLOW;
+		
+		validCircle.setFill(color);
+		validCircle.setDisable(true);
+		isRed = !isRed;
+
+		int[] winnerMoves = Connect4Service.getWinner(rawMoves);
+		if (winnerMoves.length != 0) {
+			lbl_Message.setText("You are the winner!");
+			fpGames.getChildren().forEach(x -> {
+				Circle cir = (Circle) x;
+				cir.setDisable(true);
+			});
+		}
+	}
     @FXML
     void reset(ActionEvent event) {
+    	fpGames.getChildren().forEach(x -> {
+			Circle circle = (Circle) x;
+
+			if (circle.isDisabled())
+				circle.setDisable(false);
+			circle.setFill(Color.WHITE);
+		});
+
+    	lbl_Message.setText("");
 
     }
 
